166e98ef9fd45868a596fe2fccd43ad8331d2bed
Mikhail Garbuzov <madmxg@gmail.com>
2017-04-19 22:43:55 +0300

    this.runtime in interface

--- src/index.js
+++ src/index.js
@@ -5,16 +5,16 @@
 import BetDealer from 'bet-dealer';
 
 class BetDealerChrome extends BetDealer {
-  addListener () {
-    chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-      this.processMessage(message, this.appId, sendResponse);
-      return true;
-    });
-    super.addListener();
-  }
 
-  static send (msg, cb) {
-    chrome.runtime.sendMessage(null, msg, null, cb);
+  /*
+  Interfaces:
+  - this.runtime.sendMessage - send message to bg
+  - this.runtime.onMessage.addListener - listener for forward messages
+  */
+
+  constructor (bg) {
+    super(bg);
+    this.runtime = chrome.runtime;
   }
 }
 
